515ba0cf0a80be5f02ca907456406cc9b087029d
Ratnam Tatavarty <ratnam@context360.com>
2014-11-04 14:19:04 -0800

    added deploy from scm

--- c360-harvest/recipes/c360-deploy.rb
+++ c360-harvest/recipes/c360-deploy.rb
@@ -1,7 +1,31 @@
+#app_root = "#{deploy[:deploy_to]}/current"
+#node[:deploy].each do |application, deploy|
+#  execute "mkdir #{app_root}" do
+#  execute "chmod -R g+rw #{app_root}" do
+#  end
+#end
+#
+#git "#{deploy[:deploy_to]}/current" do
+#  repository "git://github.com/opscode/chef.git"
+#  reference "master"
+#  action :sync
+#end
+#
+
+include_recipe 'deploy'
+
 node[:deploy].each do |application, deploy|
-  app_root = "#{deploy[:deploy_to]}/current"
-  execute "chmod -R g+rw #{app_root}" do
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
   end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
 end
 
 log "c360 app deployed"
