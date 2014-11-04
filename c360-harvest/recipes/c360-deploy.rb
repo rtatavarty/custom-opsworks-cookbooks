11d8514c9940a1701ab4a31f118316e62563ea9f
Ratnam Tatavarty <ratnam@context360.com>
2014-11-04 00:59:01 -0800

    add deploy recipe stub

---
+++ c360-harvest/recipes/c360-deploy.rb
@@ -0,0 +1,7 @@
+node[:deploy].each do |application, deploy|
+  app_root = "#{deploy[:deploy_to]}/current"
+  execute "chmod -R g+rw #{app_root}" do
+  end
+end
+
+log "c360 app deployed"
---
+++ c360-harvest/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: c360-harvest
+# Recipe:: default
+#
+# Copyright (c) 2014 The Authors, All Rights Reserved.
